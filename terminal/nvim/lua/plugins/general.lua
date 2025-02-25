3d7d561dce78a0689c9b8d647b3882af39247112
Max Farver <mfarver99@gmail.com>
2025-02-25 09:03:32 -0600

    renaming file and adding proto lsp

---
+++ terminal/nvim/lua/plugins/general.lua
@@ -0,0 +1,44 @@
+-- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
+--
+-- In your plugin files, you can:
+-- * add extra plugins
+-- * disable/enabled LazyVim plugins
+-- * override the configuration of LazyVim plugins
+return {
+  { "RRethy/vim-illuminate", lazy = true },
+
+  -- add pyright to lspconfig
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        -- pyright will be automatically installed with mason and loaded with lspconfig
+        buf_ls = {},
+        pyright = {},
+      },
+    },
+  },
+
+  -- add more treesitter parsers
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+    },
+  },
+}
