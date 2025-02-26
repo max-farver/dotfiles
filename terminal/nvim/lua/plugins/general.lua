c9e77fe8c1cc4fefca03d45e5964e281c11e8c33
Max Farver <mfarver99@gmail.com>
2025-02-26 09:34:47 -0600

    condense games to one file and add arrow

--- terminal/nvim/lua/plugins/general.lua
+++ terminal/nvim/lua/plugins/general.lua
@@ -5,7 +5,41 @@
 -- * disable/enabled LazyVim plugins
 -- * override the configuration of LazyVim plugins
 return {
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+      "TmuxNavigatorProcessList",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  },
+
   { "RRethy/vim-illuminate", lazy = true },
+
+  -- arrow for bookmarking
+  {
+    "otavioschwanck/arrow.nvim",
+    dependencies = {
+      { "nvim-tree/nvim-web-devicons" },
+      -- or if using `mini.icons`
+      -- { "echasnovski/mini.icons" },
+    },
+    opts = {
+      show_icons = true,
+      leader_key = "M", -- Recommended to be a single key
+      buffer_leader_key = "m", -- Per Buffer Mappings
+    },
+  },
 
   -- add pyright to lspconfig
   {
--- terminal/nvim/lua/plugins/themes.lua
+++ terminal/nvim/lua/plugins/themes.lua
@@ -1,35 +1,4 @@
 return {
   { "nyoom-engineering/oxocarbon.nvim", name = "oxocarbon" },
   { "catppuccin/nvim", name = "catppuccin" },
-
-  {
-    "zaldih/themery.nvim",
-    lazy = false,
-    config = function()
-      require("themery").setup({
-        themes = {
-          {
-            name = "Catppuccin Day",
-            colorscheme = "catppuccin-latte",
-            before = [[ vim.opt.background = "light" ]],
-          },
-          {
-            name = "Catppuccin Night",
-            colorscheme = "catppuccin-frappe",
-            before = [[ vim.opt.background = "dark" ]],
-          },
-          {
-            name = "Oxocarbon Day",
-            colorscheme = "oxocarbon",
-            before = [[ vim.opt.background = "light" ]],
-          },
-          {
-            name = "Oxocarbon Night",
-            colorscheme = "oxocarbon",
-            before = [[ vim.opt.background = "dark" ]],
-          },
-        },
-      })
-    end,
-  },
 }
