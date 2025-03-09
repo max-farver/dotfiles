511ecf48429c06a0b0b66e10a92a6af176c0c2ab
Max Farver <mfarver99@gmail.com>
2025-03-09 12:16:50 -0500

    updates

--- terminal/nvim/lua/plugins/general.lua
+++ terminal/nvim/lua/plugins/general.lua
@@ -23,23 +23,57 @@ return {
       { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
     },
   },
+  {
+    "tris203/precognition.nvim",
+    --event = "VeryLazy",
+    opts = {
+      -- startVisible = true,
+      -- showBlankVirtLine = true,
+      -- highlightColor = { link = "Comment" },
+      -- hints = {
+      --      Caret = { text = "^", prio = 2 },
+      --      Dollar = { text = "$", prio = 1 },
+      --      MatchingPair = { text = "%", prio = 5 },
+      --      Zero = { text = "0", prio = 1 },
+      --      w = { text = "w", prio = 10 },
+      --      b = { text = "b", prio = 9 },
+      --      e = { text = "e", prio = 8 },
+      --      W = { text = "W", prio = 7 },
+      --      B = { text = "B", prio = 6 },
+      --      E = { text = "E", prio = 5 },
+      -- },
+      -- gutterHints = {
+      --     G = { text = "G", prio = 10 },
+      --     gg = { text = "gg", prio = 9 },
+      --     PrevParagraph = { text = "{", prio = 8 },
+      --     NextParagraph = { text = "}", prio = 8 },
+      -- },
+      -- disabled_fts = {
+      --     "startify",
+      -- },
+    },
+  },
+  {
+    "sphamba/smear-cursor.nvim",
+    opts = {},
+  },
 
   { "RRethy/vim-illuminate", lazy = true },
 
   -- arrow for bookmarking
-  {
-    "otavioschwanck/arrow.nvim",
-    dependencies = {
-      { "nvim-tree/nvim-web-devicons" },
-      -- or if using `mini.icons`
-      -- { "echasnovski/mini.icons" },
-    },
-    opts = {
-      show_icons = true,
-      leader_key = "M", -- Recommended to be a single key
-      buffer_leader_key = "m", -- Per Buffer Mappings
-    },
-  },
+  -- {
+  --   "otavioschwanck/arrow.nvim",
+  --   dependencies = {
+  --     { "nvim-tree/nvim-web-devicons" },
+  --     -- or if using `mini.icons`
+  --     -- { "echasnovski/mini.icons" },
+  --   },
+  --   opts = {
+  --     show_icons = true,
+  --     leader_key = "M", -- Recommended to be a single key
+  --     buffer_leader_key = "m", -- Per Buffer Mappings
+  --   },
+  -- },
 
   -- add pyright to lspconfig
   {
--- terminal/nvim/lua/plugins/themes.lua
+++ terminal/nvim/lua/plugins/themes.lua
@@ -1,4 +1,10 @@
 return {
   { "nyoom-engineering/oxocarbon.nvim", name = "oxocarbon" },
   { "catppuccin/nvim", name = "catppuccin" },
+  {
+    "scottmckendry/cyberdream.nvim",
+  },
+  { "EdenEast/nightfox.nvim" },
+  { "sainnhe/everforest" },
+  { "rebelot/kanagawa.nvim" },
 }
