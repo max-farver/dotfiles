90ba2ecba5d8156f4b06db6e8fc3707ca1f6de28
Max Farver <mfarver99@gmail.com>
2025-03-09 21:54:10 -0500

    updates

--- terminal/nvim/lua/plugins/general.lua
+++ terminal/nvim/lua/plugins/general.lua
@@ -23,36 +23,36 @@ return {
       { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
     },
   },
-  {
-    "tris203/precognition.nvim",
-    --event = "VeryLazy",
-    opts = {
-      -- startVisible = true,
-      -- showBlankVirtLine = true,
-      -- highlightColor = { link = "Comment" },
-      -- hints = {
-      --      Caret = { text = "^", prio = 2 },
-      --      Dollar = { text = "$", prio = 1 },
-      --      MatchingPair = { text = "%", prio = 5 },
-      --      Zero = { text = "0", prio = 1 },
-      --      w = { text = "w", prio = 10 },
-      --      b = { text = "b", prio = 9 },
-      --      e = { text = "e", prio = 8 },
-      --      W = { text = "W", prio = 7 },
-      --      B = { text = "B", prio = 6 },
-      --      E = { text = "E", prio = 5 },
-      -- },
-      -- gutterHints = {
-      --     G = { text = "G", prio = 10 },
-      --     gg = { text = "gg", prio = 9 },
-      --     PrevParagraph = { text = "{", prio = 8 },
-      --     NextParagraph = { text = "}", prio = 8 },
-      -- },
-      -- disabled_fts = {
-      --     "startify",
-      -- },
-    },
-  },
+  -- {
+  --   "tris203/precognition.nvim",
+  --   --event = "VeryLazy",
+  --   opts = {
+  --     -- startVisible = true,
+  --     -- showBlankVirtLine = true,
+  --     -- highlightColor = { link = "Comment" },
+  --     -- hints = {
+  --     --      Caret = { text = "^", prio = 2 },
+  --     --      Dollar = { text = "$", prio = 1 },
+  --     --      MatchingPair = { text = "%", prio = 5 },
+  --     --      Zero = { text = "0", prio = 1 },
+  --     --      w = { text = "w", prio = 10 },
+  --     --      b = { text = "b", prio = 9 },
+  --     --      e = { text = "e", prio = 8 },
+  --     --      W = { text = "W", prio = 7 },
+  --     --      B = { text = "B", prio = 6 },
+  --     --      E = { text = "E", prio = 5 },
+  --     -- },
+  --     -- gutterHints = {
+  --     --     G = { text = "G", prio = 10 },
+  --     --     gg = { text = "gg", prio = 9 },
+  --     --     PrevParagraph = { text = "{", prio = 8 },
+  --     --     NextParagraph = { text = "}", prio = 8 },
+  --     -- },
+  --     -- disabled_fts = {
+  --     --     "startify",
+  --     -- },
+  --   },
+  -- },
   {
     "sphamba/smear-cursor.nvim",
     opts = {},
@@ -61,19 +61,37 @@ return {
   { "RRethy/vim-illuminate", lazy = true },
 
   -- arrow for bookmarking
-  -- {
-  --   "otavioschwanck/arrow.nvim",
-  --   dependencies = {
-  --     { "nvim-tree/nvim-web-devicons" },
-  --     -- or if using `mini.icons`
-  --     -- { "echasnovski/mini.icons" },
-  --   },
-  --   opts = {
-  --     show_icons = true,
-  --     leader_key = "M", -- Recommended to be a single key
-  --     buffer_leader_key = "m", -- Per Buffer Mappings
-  --   },
-  -- },
+  {
+    "otavioschwanck/arrow.nvim",
+    dependencies = {
+      { "nvim-tree/nvim-web-devicons" },
+      -- or if using `mini.icons`
+      -- { "echasnovski/mini.icons" },
+    },
+    opts = {
+      show_icons = true,
+      leader_key = "M", -- Recommended to be a single key
+      buffer_leader_key = "m", -- Per Buffer Mappings
+    },
+  },
+
+  -- loading dotenv files
+  {
+    "ellisonleao/dotenv.nvim",
+    lazy = false,
+    -- I don't know why, but this is required or it won't load.
+    opts = {},
+    config = function(_, opts)
+      require("dotenv").setup(opts)
+      -- load dotenv files on startup
+      local env_file_names = { ".env.base", ".env" }
+      for _, filename in ipairs(env_file_names) do
+        if vim.uv.fs_stat(filename) then
+          vim.cmd.Dotenv(filename)
+        end
+      end
+    end,
+  },
 
   -- add pyright to lspconfig
   {
