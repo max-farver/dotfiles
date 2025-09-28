af369c469c2f07e15d5c956f802742a6b22c60c4
max.farver <maxwell.farver@gmail.com>
2025-09-28 16:25:57 -0500

    nixos updates

--- nixos/terminal/default.nix
+++ nixos/terminal/default.nix
@@ -43,7 +43,35 @@ in
     ghostty = {
       enable = true;
       settings = {
-        theme = "Dracula";
+        theme = "dracula-local";
+      };
+      themes = {
+        dracula-local = {
+          background = "#282a36";
+          foreground = "#f8f8f2";
+          cursor-color = "#f8f8f2";
+          cursor-text = "#282a36";
+          selection-foreground = "#f8f8f2";
+          selection-background = "#44475a";
+          palette = [
+            "0=#21222c"
+            "1=#ff5555"
+            "2=#50fa7b"
+            "3=#f1fa8c"
+            "4=#bd93f9"
+            "5=#ff79c6"
+            "6=#8be9fd"
+            "7=#f8f8f2"
+            "8=#6272a4"
+            "9=#ff6e6e"
+            "10=#69ff94"
+            "11=#ffffa5"
+            "12=#d6acff"
+            "13=#ff92df"
+            "14=#a4ffff"
+            "15=#ffffff"
+          ];
+        };
       };
     };
 
