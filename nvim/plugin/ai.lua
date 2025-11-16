f061eae7d655e5e4d51454c8cbb3b7e1efa7411d
max.farver <maxwell.farver@gmail.com>
2025-11-15 20:30:39 -0600

    disable next edit suggestions in nvim

--- nvim/plugin/ai.lua
+++ nvim/plugin/ai.lua
@@ -28,9 +28,12 @@ end)
 later(function()
 	add({
 		source = "folke/sidekick.nvim",
-		depends = {"copilot-nvim/copilot-lsp"},
+		depends = { "copilot-nvim/copilot-lsp" },
 	})
 	require("sidekick").setup({
+		nes = {
+			enabled = false,
+		},
 		cli = {
 			mux = {
 				backend = "tmux",
@@ -42,11 +45,14 @@ later(function()
 	local map = vim.keymap.set
 	map({ "n", "t", "i", "x" }, "<c-.>", function() require("sidekick.cli").toggle() end, { desc = "Sidekick Toggle" })
 	map("n", "<leader>aa", function() require("sidekick.cli").toggle() end, { desc = "Sidekick Toggle CLI" })
-	map("n", "<leader>as", function() require("sidekick.cli").select({ filter = { installed = true } }) end, { desc = "Select CLI" })
+	map("n", "<leader>as", function() require("sidekick.cli").select({ filter = { installed = true } }) end,
+		{ desc = "Select CLI" })
 	map("n", "<leader>ad", function() require("sidekick.cli").close() end, { desc = "Detach a CLI Session" })
-	map({ "x", "n" }, "<leader>at", function() require("sidekick.cli").send({ msg = "{this}" }) end, { desc = "Send This" })
+	map({ "x", "n" }, "<leader>at", function() require("sidekick.cli").send({ msg = "{this}" }) end,
+		{ desc = "Send This" })
 	map("n", "<leader>af", function() require("sidekick.cli").send({ msg = "{file}" }) end, { desc = "Send File" })
-	map("x", "<leader>av", function() require("sidekick.cli").send({ msg = "{selection}" }) end, { desc = "Send Visual Selection" })
+	map("x", "<leader>av", function() require("sidekick.cli").send({ msg = "{selection}" }) end,
+		{ desc = "Send Visual Selection" })
 	map({ "n", "x" }, "<leader>ap", function() require("sidekick.cli").prompt() end, { desc = "Sidekick Select Prompt" })
 	map("n", "<tab>", function()
 		if not require("sidekick").nes_jump_or_apply() then
