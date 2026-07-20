55471b4e6a246a4ddaa2516c27e42cbdc0b3298c
max-farver <maxwell.farver@gmail.com>
2026-07-20 11:55:04 -0500

    Restore homelab sudo user

--- nixos/system-specific/x86_64-linux/server.nix
+++ nixos/system-specific/x86_64-linux/server.nix
@@ -15,6 +15,11 @@
     vim
   ];
 
+  users.users.mfarver = {
+    isNormalUser = true;
+    description = "Max Farver";
+    extraGroups = [ "wheel" ];
+  };
   time.timeZone = "UTC";
   i18n.defaultLocale = "en_US.UTF-8";
 
