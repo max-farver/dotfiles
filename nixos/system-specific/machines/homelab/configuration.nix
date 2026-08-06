b40d119a0ac056985e5c171a76087ec64fcee4a4
max-farver <maxwell.farver@gmail.com>
2026-08-06 09:20:15 -0500

    Enable Firefox on homelab

--- nixos/system-specific/machines/homelab/configuration.nix
+++ nixos/system-specific/machines/homelab/configuration.nix
@@ -17,6 +17,8 @@ in
 
   networking.hostName = "homelab";
 
+  programs.firefox.enable = true;
+
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.configurationLimit = 10;
   boot.loader.efi.canTouchEfiVariables = true;
